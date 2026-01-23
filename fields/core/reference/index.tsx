7006bc2cbb814913aeee02a8dd47164fe7594e18
Michael <michael@campbellssource.com>
2026-01-23 11:46:57 +0000

    fix required validation on a field that can have multiple values

--- fields/core/reference/index.tsx
+++ fields/core/reference/index.tsx
@@ -5,9 +5,26 @@ import { EditComponent } from "./edit-component";
 const schema = (field: Field) => {
   let zodSchema: z.ZodTypeAny = z.coerce.string();
 
-  if (field.options?.multiple) zodSchema = z.array(zodSchema);
-  
-  if (!field.required) zodSchema = zodSchema.optional();
+  if (field.options?.multiple) {
+    zodSchema = z.array(zodSchema);
+    
+    if (field.required) {
+      zodSchema = zodSchema.min(1, "This field is required");
+    }
+    
+    zodSchema = z.preprocess(
+      (val) => {
+        if (val === "" || val === null || val === undefined) return [];
+        // Ensure array values are converted to strings
+        return Array.isArray(val) ? val.map(String) : val;
+      },
+      zodSchema
+    );
+  } else {
+    if (!field.required) {
+      zodSchema = zodSchema.optional();
+    }
+  }
   
   return zodSchema;
 };
